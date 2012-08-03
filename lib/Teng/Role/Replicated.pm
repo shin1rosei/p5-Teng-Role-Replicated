1584b012a050d8190aa7be084b00d2c1bcf3fac3
Shinichiro Sei <sei@kayac.com>
2012-08-03 17:07:18 +0900

    initial commit

---
+++ lib/Teng/Role/Replicated.pm
@@ -0,0 +1,32 @@
+package Teng::Role::Replicated;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Teng::Role::Replicated -
+
+=head1 SYNOPSIS
+
+  use Teng::Role::Replicated;
+
+=head1 DESCRIPTION
+
+Teng::Role::Replicated is
+
+=head1 AUTHOR
+
+Shinichiro Sei E<lt>shin1rosei@kayac.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
